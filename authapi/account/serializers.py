4690f9ced4352fb08465d03cfb6f98cd895d7dd3
mehfooz-analyst5 <analyst5@ugspk.com>
2023-12-27 13:16:43 +0500

    Registration view done

---
+++ authapi/account/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import User
+
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+
+    password2 = serializers.CharField(style={'input_type':'password'}, write_only=True, trim_whitespace=False)
+
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'tc', 'password', 'password2']
+        extra_kwargs = {
+            'password' : {'write_only':True}
+        }
+
+    """
+    Validating Password & confirm password while registration.
+    """
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+        print(password, password2)
+
+        if password != password2:
+            raise serializers.ValidationError("Password and Confirm password does'nt match")
+
+        return attrs
+    
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
--- authapi/account/views.py
+++ authapi/account/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render, HttpResponse
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework.views import APIView
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
-# Create your views here.
+from .serializers import UserRegistrationSerializer
+from .models import User
 
 
+"""
+Making Api for User Registration and user login.
+"""
 
-def home(request):
-    return HttpResponse('Hey')
+
+class UserRegistrationView(APIView):
+
+    def post(self, request, format=None):
+        serializer = UserRegistrationSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            return Response({'mg':'Registration Successfull'}, status=HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
